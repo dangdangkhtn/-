a6838b031261e8a8dc4027d0e22b02f1171d6eca
Trương Học Đăng <dangdangkhtn@gmail.com>
2015-12-20 03:59:48 +0700

    State 1: Creation     - Some experiment

---
+++ Experiment/ScriptTemplate.py
@@ -0,0 +1,22 @@
+# Simple test script template
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+
+
+# illustrator V0.2
+# change: add param "sec" to control the duration of illustration
+def illustrator(web_driver, element, sec):
+    illus_style = "color:SeaShell;background: #D96459;outline-style: dashed;"
+    old_style = element.get_attribute("style")
+    web_driver.execute_script("arguments[0].setAttribute('style', arguments[1])", element, illus_style)
+    time.sleep(sec)
+    web_driver.execute_script("arguments[0].setAttribute('style', arguments[1])", element, old_style)
+
+# Init -----------------------------------------------------------------------------------------------------
+driver = webdriver.Firefox()
+driver.maximize_window()
+initPage = "http://truonghocdang.weebly.com"
+
+# ----------------------------------------------------------------------------------------------------------
